d3c60b901347d30143fd80eddae2807d1e7c6c0d
NateTheGreat1230 <nathanprattwashburn@gmail.com>
2023-05-18 13:27:49 -0600

    add multiplayer mode

--- src/main/java/com/example/javafinalproj/Main.java
+++ src/main/java/com/example/javafinalproj/Main.java
@@ -1,28 +1,32 @@
 package com.example.javafinalproj;
-
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
-
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws IOException {
-        //startController controls = new startController();
-        //controls.showStage();
-
-        Object start = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("start.fxml")));
-
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("start.fxml"));
         // Create a scene and place it in the stage
-        Scene scene = new Scene((Parent) start,1000,1000);
+        Scene scene = new Scene(loader.load(),1000,1000);
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("style.css")).toExternalForm());
         primaryStage.setTitle("Tic Tac Toe"); // Set the stage title
+        //this.stage.setScene(scene);
         primaryStage.setScene(scene); // Place the scene in the stage
         primaryStage.show(); // Display the stage
-
+    }
+    @FXML
+    public void startclick() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("game.fxml"));
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(loader.load(),1000,1000);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("style.css")).toExternalForm());
+        Stage stage = new Stage();
+        stage.setTitle("Game");
+        stage.setScene(scene); // Place the scene in the stage
+        stage.show(); // Display the stage
     }
 }
